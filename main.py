8017b0e284f0efc6c61625d2567be81e0515b190
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 17:04:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ async def main():
             if st.button("Показать историю ведущих"):
                 history_df = await get_leading_history(session)
                 if not history_df.empty:
-                    st.write(history_df.to_string(index=False, header=["Дата", "Имя"]))
+                    st.write(history_df.to_string(index=False))
                 else:
                     st.write("История ведущих пуста.")
                 
