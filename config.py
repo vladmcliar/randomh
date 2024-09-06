50ac652321f82b9bd864509637da2ae0645f333a
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 14:57:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ DB_NAME = 'postgres'
 CLOUD_SQL_CONNECTION_NAME = 'nmrandom:us-central1:mnrandom'
 
 def get_database_url():
-    return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@localhost/{DB_NAME}?host=/cloudsql/{CLOUD_SQL_CONNECTION_NAME}'
+    return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@35.232.235.84/{DB_NAME}?host=/cloudsql/{CLOUD_SQL_CONNECTION_NAME}'
