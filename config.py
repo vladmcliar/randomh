26541a594bf6d375c3820d4169e0576fe71e9a6e
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 16:14:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ def get_database_url():
     DB_PASS = st.secrets["DB_PASS"]
     DB_NAME = st.secrets["DB_NAME"]
     CLOUD_SQL_CONNECTION_NAME = st.secrets["CLOUD_SQL_CONNECTION_NAME"]
-    DB_HOST = "10.54.48.3"  # Укажите ваш хост
+    DB_HOST = "35.232.235.84"  # Укажите ваш хост
     return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{CLOUD_SQL_CONNECTION_NAME}'
 
