5cff2bace14c0af5ee725c855106de978953a5bb
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 10:56:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_NAME = os.getenv('DB_NAME')
-CLOUD_SQL_CONNECTION_NAME = os.getenv('CLOUD_SQL_CONNECTION_NAME')
+DB_USER = postgres
+DB_PASS = jiztix-toXnu9-qosxut
+DB_NAME = postgres
+CLOUD_SQL_CONNECTION_NAME = nmrandom:us-central1:mnrandom
 
 def get_database_url():
     return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@localhost/{DB_NAME}?host=/cloudsql/{CLOUD_SQL_CONNECTION_NAME}'
