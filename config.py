971b0ad39fa69efd913cac46e5c1fc955104872d
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 16:40:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = "pencil-volume"
+DB_USER = "postgres"
+DB_PASS = "hoWgrRiYylsODDTFQqjpqlEmZyAFrTea"
+DB_NAME = "railway"
 DB_HOST = "autorack.proxy.rlwy.net"
 DB_PORT = "11629"
 
 def get_database_url():
     return f'postgresql+asyncpg://{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
 
 
