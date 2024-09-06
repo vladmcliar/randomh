48bf5269494752f177ff0b10621f95ba2f919c7c
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 15:58:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-import os
-from dotenv import load_dotenv
 import streamlit as st
 
-load_dotenv()
-
-DB_USER = st.secrets["DB_USER"]
-DB_PASS = st.secrets["DB_PASS"]
-DB_NAME = st.secrets["DB_NAME"]
-CLOUD_SQL_CONNECTION_NAME = st.secrets["CLOUD_SQL_CONNECTION_NAME"]
-
 def get_database_url():
-    return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@10.54.48.3/{DB_NAME}'
+    DB_USER = st.secrets["DB_USER"]
+    DB_PASS = st.secrets["DB_PASS"]
+    DB_NAME = st.secrets["DB_NAME"]
+    DB_HOST = "10.54.48.3"  # Укажите ваш хост
+    
+    return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
