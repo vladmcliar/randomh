0541fcc4972c8916b4eb396a766a3904c2c55436
vladmcliar <119409917+vladmcliar@users.noreply.github.com>
2024-09-06 16:35:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DB_NAME = "pencil-volume"
 DB_HOST = "autorack.proxy.rlwy.net"
-DB_PORT = "5432"
+DB_PORT = "11629"
 
 def get_database_url():
     return f'postgresql+asyncpg://{DB_HOST}:{DB_PORT}/{DB_NAME}'
